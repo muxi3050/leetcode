ae8957d17eb45f7cef0caebc8a7aa5e3c94795b9
fyk <305075210@qq.com>
2023-01-14 18:10:30 +0800

    add leetcode27

---
+++ 01_array/27.py
@@ -0,0 +1,15 @@
+from typing import List
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        fast = 0
+        slow = 0
+        while fast < len(nums):
+            if nums[fast]!=val:
+                nums[slow]=nums[fast]
+                slow+=1
+            fast+=1
+        return slow
+s = Solution()
+nums = [0,1,2,2,3,0,4,2]
+val = 2
+print(s.removeElement(nums,val))
